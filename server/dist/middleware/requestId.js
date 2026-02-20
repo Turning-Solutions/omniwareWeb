2f89a82aec45f5b83ff80becd4162ab3e819e7c5
turingsolutionsdev <turingsolutionsltd@gmail.com>
2026-02-20 18:48:28 +0530

    Delete server directory

--- server/dist/middleware/requestId.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.requestIdMiddleware = void 0;
-const uuid_1 = require("uuid");
-const requestIdMiddleware = (req, res, next) => {
-    req.id = (0, uuid_1.v4)();
-    next();
-};
-exports.requestIdMiddleware = requestIdMiddleware;
--- server/src/routes/builderRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createBuildRequest } from '../controllers/builderController';
-
-const router = express.Router();
-
-router.post('/requests', createBuildRequest);
-
-export default router;
